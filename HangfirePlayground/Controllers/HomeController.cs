bd228d6b680f64237f1b90c5ba9b7f98701e4db5
mbuenoferrer <mbuenoferrer@gmail.com>
2014-09-23 15:08:45 +0200

    Upload solution

---
+++ HangfirePlayground/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Hangfire;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HangfirePlayground.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult FireAndForget()
+        {
+            BackgroundJob.Enqueue(() => Debug.WriteLine("Fire-and-forget"));
+            return Redirect("/hangfire/");
+        }
+        public ActionResult Delayed()
+        {
+            BackgroundJob.Schedule(() => Debug.WriteLine("Delayed"), TimeSpan.FromDays(1));
+            return Redirect("/hangfire/");
+        }
+        public ActionResult Recurring()
+        {
+            RecurringJob.AddOrUpdate(() => Debug.Write("Recurring"), Cron.Minutely);
+            return Redirect("/hangfire/");
+        }
+    }
+}
---
+++ HangfirePlayground/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Hangfire;
+using Hangfire.SqlServer;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HangfirePlayground.Startup))]
+
+namespace HangfirePlayground
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseHangfire(config =>
+            {
+                config.UseSqlServerStorage("Server=localhost;Database=_HangfirePlayground;Trusted_Connection=True;");
+                config.UseServer();
+            });
+        }
+
+    }
+}
